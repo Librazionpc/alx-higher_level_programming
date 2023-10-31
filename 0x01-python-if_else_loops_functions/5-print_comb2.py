12630258040b2081724aaca51d3d87b25ad648c8
Librazionpc <nwajeizion393@gmail.com>
2023-10-31 21:12:25 +0100

    First push

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if (i < 10):
-        print(f"0{i}", end=", ")
+        print("0{:d}".format(i), end=", ")
         continue
-    elif (i == 99):
-        print(i)
-        break
-    print(i, end=", ")
+    else:
+        print("{:d}".format(i), end=", " if i < 99 else "\n")
