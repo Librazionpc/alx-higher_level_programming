ef6dd257d6dac9e7a144ba7aa375a03717af66ff
Librazionpc <nwajeizion393@gmail.com>
2023-10-31 20:33:59 +0100

    First push

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if (i < 10):
+        print(f"0{i}", end=", ")
+        continue
+    elif (i == 99):
+        print(i)
+        break
+    print(i, end=", ")
