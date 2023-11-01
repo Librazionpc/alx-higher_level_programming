e5d80f946eb6bc8446a40073e4ddb4e90cb708d6
Librazionpc <nwajeizion393@gmail.com>
2023-11-01 10:20:40 +0100

    next day

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    output = ""
+    for char in str:
+        if ('a' <= char <= 'z'):
+            upper_char = chr(ord(char) - 32)
+            output += upper_char
+        else:
+            output += char
+    print(output)
