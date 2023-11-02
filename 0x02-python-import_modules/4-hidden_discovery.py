875222a9ad23a67c26d81911a1917926f36603d3
Librazionpc <nwajeizion393@gmail.com>
2023-11-02 21:58:36 +0100

    fixing Task 4 bugs

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,11 +4,9 @@ import hidden_4
 
 def main():
     lists = dir(hidden_4)
-    for i in range(len(lists)):
-        if lists[i][0] == '_' and lists[i][1] == '_':
-            continue
-        else:
-            print("{}".format(l[i]))
+    for i in lists:
+        if i[0] != "_" and i[1] != "_":
+            print(i)
 
 
 if __name__ == "__main__":
