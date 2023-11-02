a2f7cbc4d3178061a281c1a4c6db21f39d5cff73
Librazionpc <nwajeizion393@gmail.com>
2023-11-02 18:19:49 +0100

    2 fixing bugs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,11 @@ from sys import argv
 
 lent = len(argv)
 if lent == 1:
-    print("0 arguments.")
+    print("0 arguments.", end="\n")
 elif lent > 1:
     if (lent == 2):
-        print("{} argument:".format(lent - 1))
+        print("{} argument:".format(lent - 1), end="\n")
     else:
-        print("{} arguments:".format(lent - 1))
+        print("{} arguments:".format(lent - 1), end="\n")
     for i in range(1, lent):
-        print("{}: {}".format(i, argv[i]))
+        print("{}: {}".format(i, argv[i]), end="\n")
