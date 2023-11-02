53221335076d81b2ac8f411f534ee8d147e9250d
Librazionpc <nwajeizion393@gmail.com>
2023-11-03 00:07:28 +0100

    fixing Task 2 bugs

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
 
+def main():
 
-lent = len(argv)
-if lent == 1 or lent == 2:
-    statements = "argument."
-else:
-    statements = "arguments:"
-print("{:d} {}".format(lent - 1, statements))
+    lent = len(argv)
+    if lent == 1 or lent == 2:
+        statements = "argument."
+    else:
+        statements = "arguments:"
+    print("{:d} {}".format(lent - 1, statements))
 
-for i in range(1, lent):
-    print("{:d}: {}".format(i, argv[i]))
+    for i in range(1, lent):
+        print("{:d}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    main()
