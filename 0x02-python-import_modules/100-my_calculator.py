717f6368209fab56f0ae0cc7ce0d85defea3b1bc
Librazionpc <nwajeizion393@gmail.com>
2023-11-03 01:25:08 +0100

    fixing Task 2 bugs

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,20 +7,22 @@ def main():
     lent = len(argv)
     if lent != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
-    op= argv[2]
+        exit(1)
+    op = argv[2]
     a = int(argv[1])
     b = int(argv[3])
-    if (op == "+"):    
+    if (op == "+"):
         print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif (op == "-"):    
+    elif (op == "-"):
         print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif (op == "*"):    
+    elif (op == "*"):
         print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif (op == "/"):    
+    elif (op == "/"):
         print("{} {} {} = {}".format(a, op, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
+
+
 if __name__ == "__main__":
     main()
