89a1077c014736917e1cc36c40d6f7046ce6e3fa
Librazionpc <nwajeizion393@gmail.com>
2023-11-07 00:03:45 +0100

    checker task 0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,5 +7,5 @@ def print_matrix_integer(matrix=[[]]):
         if (array_of_list == []):
             continue
         for i in array_of_list:
-            print("{}".format(i), end=" ")
+            print("{:d}".format(i), end=" ")
         print("\n")
