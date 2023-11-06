6a0b405dd99cb0d44860374e0253ad078cd2af7c
Librazionpc <nwajeizion393@gmail.com>
2023-11-07 00:02:25 +0100

    checker task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,7 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    if (my_list == []):
-        return
-    for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+    for i in my_list:
+        print("{:d}".format(i), end="\n")
