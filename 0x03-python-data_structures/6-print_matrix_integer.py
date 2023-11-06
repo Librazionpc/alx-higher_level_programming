3bd4dcc196b4150f91efbbfd898254e6da352a26
Librazionpc <nwajeizion393@gmail.com>
2023-11-07 00:18:21 +0100

    checker task 0

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,4 @@ def print_matrix_integer(matrix=[[]]):
             if array_of_list is not None:
                 for i in array_of_list:
                     print("{:d}".format(i), end=" ")
-                print("")
+                print("", end="")
