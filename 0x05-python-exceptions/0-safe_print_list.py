c05dbd153bb2fc7ea63e0dd3819a515d564f486c
Librazionpc <nwajeizion393@gmail.com>
2023-11-21 01:11:40 +0100

    Exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    no_of_printed_element = 0
+
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+            no_of_printed_element += 1
+    except (IndexError, TypeError):
+        pass
+    finally:
+        print()
+    return (no_of_printed_element)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Dealing with try / exceptions
