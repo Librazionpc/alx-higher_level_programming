3f1f3bf18087e17f7f194c9ec30263f2a2f0beb3
Librazionpc <nwajeizion393@gmail.com>
2023-11-21 01:27:01 +0100

    2-checker

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,7 +10,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 int_printed += 1
     except (TypeError, ValueError):
         pass
-    except (IndexError):
-        raise
     print()
     return (int_printed)
