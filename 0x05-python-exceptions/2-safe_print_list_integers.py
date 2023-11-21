b722f490adc87c91798f756377f9f06a55fc8a3b
Librazionpc <nwajeizion393@gmail.com>
2023-11-21 01:15:53 +0100

    2-checker

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if type(my_list[i]) == int:
                 print("{:d}".format(my_list[i]), end="")
                 int_printed += 1
-    except (TypeError):
+    except (TypeError, ValueError):
         pass
     finally:
         print()
