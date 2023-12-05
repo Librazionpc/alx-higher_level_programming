9a395915b4da06b6cfca569411b3a280e5af9a3a
Librazionpc <nwajeizion393@gmail.com>
2023-12-05 01:25:27 +0100

    Done

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -12,11 +12,11 @@ class Rectangle(BaseGeometry):
         """Initialize Rectangle"""
         if (type(width) != int):
             raise TypeError("width must be an integer")
-        if (width < 0):
+        if (width <= 0):
             raise ValueError("width must be greater than 0")
         if (type(height) != int):
             raise TypeError("height must be an integer")
-        if (height < 0):
+        if (height <= 0):
             raise ValueError("height must be greater than 0")
         self.__width = width
         self.__height = height
