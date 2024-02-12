ee28b90bd230d10c63f4d6ba01834f8647b9a6b9
Librazionpc <nwajeizion393@gmail.com>
2024-02-13 00:40:49 +0100

    dealing with convension

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+const x = process.argv[2];
+
+if (!parseInt(x)) {
   console.log('Missing number of occurrences');
 } else {
-  const x = parseInt(process.argv[2]);
-  if (x) {
-    for (let i = 0; i < x; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
