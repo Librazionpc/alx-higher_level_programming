0693ba46f51d9df57f04f60480fff2efad123dc4
Librazionpc <nwajeizion393@gmail.com>
2024-02-13 18:52:09 +0100

    Fixed remeaing issues

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-const array1 = [];
-if (process.argv.length === 2 || process.argv[2] === '1')
-        console.log(0);
-else
-{
-	const args = process.argv
-	.map(Number)
-	.slice(2, process.argv.length)
-	.sort((a, b) => a - b);
-	console.log(args[args.length - 2]);
+if (process.argv.length === 2 || process.argv[2] === '1') {
+  console.log(0);
+} else {
+  const args = process.argv
+    .map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const x = process.argv[2];
-
 if (!parseInt(x)) {
   console.log('Missing number of occurrences');
 } else {
