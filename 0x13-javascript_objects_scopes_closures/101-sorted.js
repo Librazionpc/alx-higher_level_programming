c5bcb2fe139efc739361eb21209f9c171f8decc8
Librazionpc <nwajeizion393@gmail.com>
2024-03-12 19:28:05 +0100

    Fixing Naming issuse

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const { dict } = require('./101-main.js').dict;
+const { dict } = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
