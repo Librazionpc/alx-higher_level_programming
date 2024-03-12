195c76ba332753efce620e2ab53b25a6190dc81f
Librazionpc <nwajeizion393@gmail.com>
2024-03-12 12:50:56 +0100

    Debugging

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const { dict } = require('./data');
+const { dict } = require('./101-main.js');
 const UserOccurrenceDict = [];
 for (const key in dict) {
   const value = dict[key];
-
   if (!UserOccurrenceDict[value]) {
     UserOccurrenceDict[value] = [];
   }
