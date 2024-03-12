9a77c0d0048428e48281232149d6ee49500a6376
Librazionpc <nwajeizion393@gmail.com>
2024-03-12 19:32:42 +0100

    Hmmm.

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@
 const { dict } = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-  const value = dict[key]
+  const value = dict[key];
   if (newDict[value] === undefined) {
     newDict[value] = [];
   } else {
