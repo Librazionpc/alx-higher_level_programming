49cc5558142a8b11f0d8b2e0ab5a1f237e0fe3fd
Librazionpc <nwajeizion393@gmail.com>
2024-03-12 19:31:18 +0100

    Fixing Naming issuse

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,10 +3,11 @@
 const { dict } = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+  const value = dict[key]
+  if (newDict[value] === undefined) {
+    newDict[value] = [];
   } else {
-    newDict[dict[key]].push(key);
+    newDict[value].push(key);
   }
 }
 console.log(newDict);
