02003c5d86aee69111462c9e664969294cd5fcae
Librazionpc <nwajeizion393@gmail.com>
2024-03-12 19:19:51 +0100

    Fixing Sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,8 +4,8 @@ const { dict } = require('./101-main.js');
 const UserOccurrenceDict = [];
 for (const key in dict) {
   const value = dict[key];
-  if (!UserOccurrenceDict[value]) {
-    UserOccurrenceDict[value] = [];
+  if (UserOccurrenceDict[value] === undefinded) {
+    UserOccurrenceDict[value] = [Key];
   }
 
   UserOccurrenceDict[value].push(key);
