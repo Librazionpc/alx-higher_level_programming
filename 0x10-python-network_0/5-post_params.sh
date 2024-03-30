b35958cf6d8dbb228aefbe2a3ee0cfdddaecc539
Librazionpc <nwajeizion393@gmail.com>
2024-03-30 20:06:06 +0100

    Fixing Issues

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Scripts that POST to an email address with a body
-curl -s "$1" -X POST "email=test@gmail.com&subjec=I will always be here for PLD"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
