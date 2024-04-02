3630f3f73b92beb551a3f1a03ac30e3a7d27de11
Librazionpc <nwajeizion393@gmail.com>
2024-04-02 03:07:21 +0100

    All

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status
+"""
+import urllib.request
+
+if __name__ == '__main__':
+
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as result:
+        print("Body response:")
+        print("\t- type:", type(result.read()))
+        print("\t- content:", result.read())
+        print("\t- utf8 content:", result.read().decode('utf-8'))
